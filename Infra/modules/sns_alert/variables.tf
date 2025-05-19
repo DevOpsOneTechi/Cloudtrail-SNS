695ad33f8b592b594a7b164ea5f315012e90e773
techOpsbuddy <techops.buddy@gmail.com>
2025-05-19 15:26:47 +0530

    Update variables.tf

--- Infra/modules/sns_alert/variables.tf
+++ Infra/modules/sns_alert/variables.tf
@@ -11,3 +11,13 @@ variable "evaluation_periods" {
   description = "Number of periods for alarm evaluation"
   type        = number
 }
+
+variable "metric_name" {
+  description = "The name of the CloudWatch metric"
+  type        = string
+}
+
+variable "metric_namespace" {
+  description = "The namespace for the CloudWatch metric"
+  type        = string
+}
