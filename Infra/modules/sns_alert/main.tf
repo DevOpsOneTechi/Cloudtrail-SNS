618d2e3cb5a93d8e8a4a8fdf2b6e3c9011a558af
techOpsbuddy <techops.buddy@gmail.com>
2025-05-19 15:25:17 +0530

    Update main.tf

--- Infra/modules/sns_alert/main.tf
+++ Infra/modules/sns_alert/main.tf
@@ -17,5 +17,12 @@ resource "aws_sns_topic_subscription" "emails" {
 resource "aws_cloudwatch_metric_alarm" "attach_sns" {
   alarm_name          = var.alarm_name
   alarm_actions       = [aws_sns_topic.alerts.arn]
-  arn                 = var.alarm_arn
+  comparison_operator = "GreaterThanOrEqualToThreshold"  # Example value
+  evaluation_periods  = 1  # Example value
+  threshold           = 1  # Adjust as needed
+  metric_name         = var.metric_name
+  namespace           = var.metric_namespace
+  statistic           = "Sum"
+  period              = 300
 }
+
