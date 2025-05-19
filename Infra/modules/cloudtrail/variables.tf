e1daa65af258ce0d15663e854ab12931764b2ac5
techOpsbuddy <techops.buddy@gmail.com>
2025-05-19 13:53:11 +0530

    Update variables.tf

--- Infra/modules/cloudtrail/variables.tf
+++ Infra/modules/cloudtrail/variables.tf
@@ -1,13 +1,13 @@
 variable "trail_name" {
-
   type = string
-
 }
 
- 
 
 variable "cw_log_group_name" {
-
   type = string
+}
 
+variable "s3_bucket_name" {
+  description = "The S3 bucket where CloudTrail logs will be stored"
+  type        = string
 }
