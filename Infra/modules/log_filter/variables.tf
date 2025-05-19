dac6451e1cfbb0e99bcfa5e025ed5e208cde5d0d
techOpsbuddy <techops.buddy@gmail.com>
2025-05-19 14:20:46 +0530

    Update variables.tf

--- Infra/modules/log_filter/variables.tf
+++ Infra/modules/log_filter/variables.tf
@@ -5,3 +5,8 @@ variable "filter_pattern" { type = string }
 variable "metric_namespace" { type = string }
 
 variable "metric_name" { type = string }
+
+variable "alarm_actions" {
+  description = "List of actions to trigger when alarm is triggered"
+  type        = list(string)
+}
