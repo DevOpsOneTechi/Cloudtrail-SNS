560ee3d3e892f4ae046a16b768f7b208c97a5e21
techOpsbuddy <techops.buddy@gmail.com>
2025-05-19 17:41:16 +0530

    Update main.tf

--- Infra/modules/s3_bucket/main.tf
+++ Infra/modules/s3_bucket/main.tf
@@ -7,44 +7,37 @@ resource "aws_s3_bucket" "cloudtrail_logs" {
 }
 
 resource "aws_s3_bucket_public_access_block" "block" {
-  bucket = aws_s3_bucket.cloudtrail_logs.id
+  bucket                  = aws_s3_bucket.cloudtrail_logs.id
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
-
 }
-
- 
 
 resource "aws_s3_bucket_policy" "cloudtrail_policy" {
   bucket = aws_s3_bucket.cloudtrail_logs.id
   policy = data.aws_iam_policy_document.cloudtrail_s3_policy.json
 }
 
- 
-
 data "aws_iam_policy_document" "cloudtrail_s3_policy" {
   statement {
     sid = "AWSCloudTrailWrite"
+
     principals {
       type        = "Service"
       identifiers = ["cloudtrail.amazonaws.com"]
     }
 
-    # Specify actions
     actions = [
-      "s3:GetBucketAcl",   # Only for bucket ARN
-      "s3:PutObject"       # For objects in the bucket
+      "s3:GetBucketAcl", # Bucket-level action
+      "s3:PutObject"     # Object-level action
     ]
 
-    # Define separate resource ARNs
     resources = [
-      aws_s3_bucket.cloudtrail_logs.arn,           # Bucket-level action (GetBucketAcl)
-      "${aws_s3_bucket.cloudtrail_logs.arn}/*"    # Object-level action (PutObject)
+      aws_s3_bucket.cloudtrail_logs.arn,        # For s3:GetBucketAcl
+      "${aws_s3_bucket.cloudtrail_logs.arn}/*" # For s3:PutObject
     ]
 
-    # Condition for PutObject
     condition {
       test     = "StringEquals"
       variable = "s3:x-amz-acl"
