72417f040cd5f8820660edd83e8634d56640c5d0
techOpsbuddy <techops.buddy@gmail.com>
2025-05-19 12:28:19 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.10.0"
 
   backend "s3" {
-    bucket       = "techopsbucket123"
+    bucket       = "mycloudtrailbucket123"
     key          = "vpcec2/terraform.tfstateec21"
     region       = "us-east-1"
     encrypt      = true
