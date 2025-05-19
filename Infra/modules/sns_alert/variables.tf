a173bb358d9b59d6cbe8410fc4325b7edba550a8
techOpsbuddy <techops.buddy@gmail.com>
2025-05-19 15:20:16 +0530

    Update variables.tf

--- Infra/modules/sns_alert/variables.tf
+++ Infra/modules/sns_alert/variables.tf
@@ -2,3 +2,12 @@ variable "topic_name" { type = string }
 variable "email_addresses" { type = list(string) }
 variable "alarm_name" { type = string }
 variable "alarm_arn" { type = string }
+variable "comparison_operator" {
+  description = "Comparison operator for CloudWatch alarm"
+  type        = string
+}
+
+variable "evaluation_periods" {
+  description = "Number of periods for alarm evaluation"
+  type        = number
+}
