87cdf08767f44593d3d2f366fbd56525dfe8a6cf
techOpsbuddy <techops.buddy@gmail.com>
2025-05-19 12:20:10 +0530

    Update variables.tf

--- Infra/modules/variables.tf
+++ Infra/modules/variables.tf
@@ -1,7 +1,56 @@
-variable "topic_name" { type = string }
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
 
-variable "email_addresses" { type = list(string) }
+ 
+variable "trail_name" {
+  description = "Name of the CloudTrail trail"
+  type        = string
+  default     = "all-account-activity"
+}
 
-variable "alarm_name" { type = string }
+variable "cloudwatch_log_group_name" {
+  description = "Name of the CloudWatch Log Group for CloudTrail"
+  type        = string
+  default     = "/aws/cloudtrail/all"
+}
 
-variable "alarm_arn" { type = string }
+ 
+variable "login_filter_pattern" {
+  description = "Filter pattern to detect AWS Console login events"
+  type        = string
+  default     = "{ $.eventName = \"ConsoleLogin\" && $.errorMessage = \"Successful\" }"
+}
+
+ 
+
+variable "metric_namespace" {
+  description = "CloudWatch metric namespace"
+  type        = string
+  default     = "LoginAlerts"
+}
+
+ 
+
+variable "metric_name" {
+  description = "CloudWatch metric name for login events"
+  type        = string
+  default     = "SuccessfulConsoleLogins"
+}
+
+ 
+
+variable "sns_topic_name" {
+  description = "SNS Topic name for alerts"
+  type        = string
+  default     = "login-alerts-topic"
+}
+
+ 
+
+variable "notification_emails" {
+  description = "List of emails to notify"
+  type        = list(string)
+}
