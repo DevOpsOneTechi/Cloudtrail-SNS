50e997da5725783f9935122ef7b9708fde6db96c
techOpsbuddy <techops.buddy@gmail.com>
2025-05-19 12:17:46 +0530

    Update main.tf

---
+++ Infra/modules/sns_alert/main.tf
@@ -0,0 +1,31 @@
+resource "aws_sns_topic" "alerts" {
+
+  name = var.topic_name
+
+}
+
+ 
+
+resource "aws_sns_topic_subscription" "emails" {
+
+  for_each = toset(var.email_addresses)
+
+  topic_arn = aws_sns_topic.alerts.arn
+
+  protocol  = "email"
+
+  endpoint  = each.value
+
+}
+
+ 
+
+resource "aws_cloudwatch_metric_alarm" "attach_sns" {
+
+  alarm_name          = var.alarm_name
+
+  alarm_actions       = [aws_sns_topic.alerts.arn]
+
+  arn                 = var.alarm_arn
+
+}
