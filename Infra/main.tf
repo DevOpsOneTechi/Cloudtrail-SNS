156c824a7a26362f157a3170afd8d773d630f131
techOpsbuddy <techops.buddy@gmail.com>
2025-05-19 14:07:40 +0530

    Update main.tf

--- Infra/main.tf
+++ Infra/main.tf
@@ -8,7 +8,7 @@ module "cloudtrail" {
   source              = "./modules/cloudtrail"
   trail_name          = var.trail_name
   cw_log_group_name   = var.cloudwatch_log_group_name
-  s3_bucket_name = var.s3_bucket_name
+  s3_bucket_name      = module.s3_bucket.bucket_name
 }
 
  
