ae363f488f979f4e1c0ddc694a909655b0d8c2f4
techOpsbuddy <techops.buddy@gmail.com>
2025-05-19 12:36:18 +0530

    Update main.tf

--- Infra/main.tf
+++ Infra/main.tf
@@ -1,33 +1,21 @@
 resource "aws_sns_topic" "alerts" {
-
   name = var.topic_name
-
 }
 
  
 
 resource "aws_sns_topic_subscription" "emails" {
-
   for_each = toset(var.email_addresses)
-
   topic_arn = aws_sns_topic.alerts.arn
-
   protocol  = "email"
-
   endpoint  = each.value
-
 }
 
  
 
 resource "aws_cloudwatch_metric_alarm" "attach_sns" {
-
   alarm_name          = var.alarm_name
-
   alarm_actions       = [aws_sns_topic.alerts.arn]
-
   arn                 = var.alarm_arn
-
   # Note: in Terraform you cannot attach actions to an existing alarm; recommend import or recreate in this module context
-
 }
